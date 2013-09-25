8b44239efa9f9e039d81a68c13333d517a5fce19
MattCowski <MattCowski@gmail.com>
2013-09-24 23:30:12 -0500

    'new task' will now render within same page (jquery)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoLater::Application.config.secret_key_base = '2436f84db16cf6906eccdc1d34baf72eddad6c4794127837e6e9d48577b6ab30ef46ceb7bbdc3700c1602801056d46e98fb6a76c74b5231290f0165b1f798a8c'
